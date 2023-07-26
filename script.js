ba7668d0d3d75d54359afd75ead1872fd35b3a68
ebonyhunt <107260057+ebonyhunt@users.noreply.github.com>
2023-07-26 14:04:16 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,23 @@
+$(".orange").click(function() {
+    $(".blue").toggle();
+    $(".correct").toggle();
+    $(".poison").toggle();
+    $(".orange").toggle();
+
+});
+$(".blue").click(function() {
+    $(".blue").toggle();
+    $(".poison1").toggle();
+    $(".wrong").toggle();
+    $(".orange").toggle();
+    
+
+});
+
+$(".purple").click(function() {
+    $(".purple").toggle();
+    $(".poison1").toggle();
+    $(".wrong").toggle();
+    $(".purple").toggle();
+
+});
